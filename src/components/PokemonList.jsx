99cc172288ad505d8f3973963df00de719e85c31
ye21iin <kathykr120@gmail.com>
2025-02-04 13:01:21 +0900

    Design: 포켓몬 목록 컴포넌트에 스타일 추가 및 props를 통해 포켓몬 카드 데이터 전달

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import PokemonCard from "./PokemonCard";
+import styled from "styled-components";
 
-const PokemonList = () => {
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const PokemonList = ({ pokemons }) => {
   return (
-    <>
-      <PokemonCard></PokemonCard>
-    </>
+    <ListContainer>
+      {pokemons.map((mon) => {
+        const props = {
+          IMG_SRC: mon.img_url,
+          name: mon.korean_name,
+          id: mon.id,
+        };
+        return <PokemonCard key={mon.id} {...props}></PokemonCard>;
+      })}
+    </ListContainer>
   );
 };
 
