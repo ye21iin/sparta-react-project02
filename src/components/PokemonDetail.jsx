c2b1cea97943293504b1bf9e5597a96b961cb9d3
ye21iin <kathykr120@gmail.com>
2025-02-07 14:57:14 +0900

    Feat: Detail 페이지 내부 삭제 기능 구현

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -1,4 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { handleAdd, handleDelete } from "../redux/slice";
 import {
   DetailContainer,
   Img,
@@ -7,20 +9,29 @@ import {
   BackBtn,
   BtnContainer,
 } from "../style";
-import MOCK_DATA from "../data/mockData";
-import { useDispatch } from "react-redux";
-import { handleAdd } from "../redux/slice";
 
 const PokemonDetail = () => {
-  const navigate = useNavigate();
   const queryParams = new URLSearchParams(useLocation().search);
   const id = parseInt(queryParams.get("id"), 10);
 
-  const dataset = MOCK_DATA;
-  const variable = dataset.find((data) => data.id === id);
-  const { img_url, korean_name, types, description } = variable;
-
+  const { pokemons, selected } = useSelector((state) => state.pokemon);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isSelectedHasMon = selected.find((mon) => mon.id === id);
+  const isPokemonsHasMon = pokemons.find((mon) => mon.id === id);
+  const { img_url, korean_name, types, description, isSelected } =
+    isSelectedHasMon ? isSelectedHasMon : isPokemonsHasMon;
+
+  const handleAddOnClick = () => {
+    dispatch(handleAdd(id));
+    navigate(-1);
+  };
+  const handleDeleteOnClick = () => {
+    dispatch(handleDelete(id));
+    navigate(-1);
+  };
+
   return (
     <DetailContainer>
       <Img src={img_url} alt="Pokemon" />
@@ -30,13 +41,8 @@ const PokemonDetail = () => {
         <p>{description}</p>
       </NameTag>
       <BtnContainer>
-        <Btn
-          onClick={() => {
-            dispatch(handleAdd(id));
-            navigate(-1);
-          }}
-        >
-          추가
+        <Btn onClick={isSelected ? handleDeleteOnClick : handleAddOnClick}>
+          {isSelected ? "삭제" : "추가"}
         </Btn>
         <BackBtn onClick={() => navigate(-1)}>뒤로 가기</BackBtn>
       </BtnContainer>
