0cbcf115c8c6e463e315be55044eaac61c20e0ac
ye21iin <kathykr120@gmail.com>
2025-02-07 18:00:36 +0900

    Feat: Dex 페이지 진입 시 alert 기능 추가

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -4,10 +4,16 @@ import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
 import { useDispatch, useSelector } from "react-redux";
 import { setPokemons } from "../redux/slice";
+import { toast } from "react-toastify";
 
 const Dex = () => {
   const dispatch = useDispatch();
   const pokemons = useSelector((state) => state.pokemon.pokemons);
+  useEffect(() => {
+    toast(`🎁 나만의 포켓몬 세계를 완성해보세요!`);
+    toast.info(`각 포켓몬을 클릭하면 상세 정보를 확인할 수 있습니다.`);
+  }, []);
+
   useEffect(() => {
     if (pokemons.length === 0) {
       dispatch(setPokemons(MOCK_DATA));
