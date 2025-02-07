618d69b3b4ab2478e90201ebdb49c278afb7873d
ye21iin <kathykr120@gmail.com>
2025-02-07 14:09:21 +0900

    Feat: Detail 페이지 내부 추가 기능 구현

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -8,6 +8,8 @@ import {
   BtnContainer,
 } from "../style";
 import MOCK_DATA from "../data/mockData";
+import { useDispatch } from "react-redux";
+import { handleAdd } from "../redux/slice";
 
 const PokemonDetail = () => {
   const navigate = useNavigate();
@@ -18,6 +20,7 @@ const PokemonDetail = () => {
   const variable = dataset.find((data) => data.id === id);
   const { img_url, korean_name, types, description } = variable;
 
+  const dispatch = useDispatch();
   return (
     <DetailContainer>
       <Img src={img_url} alt="Pokemon" />
@@ -27,7 +30,14 @@ const PokemonDetail = () => {
         <p>{description}</p>
       </NameTag>
       <BtnContainer>
-        <Btn>추가</Btn>
+        <Btn
+          onClick={() => {
+            dispatch(handleAdd(id));
+            navigate(-1);
+          }}
+        >
+          추가
+        </Btn>
         <BackBtn onClick={() => navigate(-1)}>뒤로 가기</BackBtn>
       </BtnContainer>
     </DetailContainer>
